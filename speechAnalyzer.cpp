8b9c17b439af198d3f8df1fbe4daf1b52f3a2f77
Vincent Raymond <vraymond6@email.arizona.edu>
2021-02-10 13:37:36 -0700

    Conformed speechAnalyzer.cpp to clang-format specifications

--- speechAnalyzer.cpp
+++ speechAnalyzer.cpp
@@ -1,34 +1,36 @@
+#include <cerrno>  //errno
+#include <cstring> //strerror
 #include <iostream>
 #include <vector>
-#include <cstring> //strerror
-#include <cerrno> //errno
 
-const size_t MAX_CHUNK_SIZE = 128;
+const size_t MAX_CHUNK_SIZE = 2048;
 
-void process_chunk(std::vector<char>);
+void process_chunk(const std::vector<unsigned char>&);
 
-int main(){	
-	try{	
-		std::freopen(nullptr, "rb", stdin); //reopen stdin in binary mode
-	
-		if(std::ferror(stdin)){
-			throw(std::runtime_error(std::strerror(errno)));
-		}
+int main() {
+    try {
+        std::freopen(nullptr, "rb", stdin); // reopen stdin in binary mode
 
-		std::vector<char> buffer(MAX_CHUNK_SIZE);
-		std::size_t length;
-		while((length = std::fread(&buffer[0], sizeof buffer[0], buffer.size(), stdin)) > 0){
-			std::vector<char> chunk(buffer.begin(), buffer.begin() + length);
-			process_chunk(chunk);
-		}
-	}
-	catch(std::exception const& e){
-		std::cerr << e.what() << std::endl;
-		return EXIT_FAILURE;
-	}
-	return EXIT_SUCCESS;
+        if (std::ferror(stdin)) {
+            throw(std::runtime_error(std::strerror(errno)));
+        }
+
+        std::vector<unsigned char> buffer(MAX_CHUNK_SIZE);
+        std::size_t length;
+        while ((length = std::fread(
+                    &buffer[0], sizeof buffer[0], buffer.size(), stdin)) > 0) {
+            std::vector<unsigned char> chunk(buffer.begin(),
+                                             buffer.begin() + length);
+            process_chunk(chunk);
+        }
+    }
+    catch (std::exception const& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
 }
 
-void process_chunk(std::vector<char> chunk){
-	std::cout << chunk.size() << std::endl;
+void process_chunk(const std::vector<unsigned char>& chunk) {
+    std::cout << chunk.size() << std::endl;
 }
