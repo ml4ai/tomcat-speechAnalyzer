be51b6352b66d1ed08e38d4228cb76585a3280a2
Vincent Raymond <vraymond6@email.arizona.edu>
2021-02-15 18:09:39 -0700

    Protoype open_smile integration for extracting energy in real time

--- src/speechAnalyzer.cpp
+++ src/speechAnalyzer.cpp
@@ -1,3 +1,5 @@
+#include "SMILEapi.h"
+#include <thread>
 #include <cerrno>  //errno
 #include <cstring> //strerror
 #include <iostream>
@@ -5,31 +7,48 @@
 
 const size_t MAX_CHUNK_SIZE = 2048;
 
-void process_chunk(const std::vector<unsigned char>&);
 
 int main() {
+
+    //Initialize and start opensmile
+    smileobj_t* handle;
+    smileopt_t options[1];
+
+    options[0].name = "O";
+    options[0].value = "opensmile.energy.csv";
+
+    handle = smile_new();
+    smile_initialize(handle, "external_audio_test.conf", 1, options, 1, 0, 1, 0);
+    std::thread thread_object(smile_run, handle);
+
     try {
         std::freopen(nullptr, "rb", stdin); // reopen stdin in binary mode
 
         if (std::ferror(stdin)) {
             throw(std::runtime_error(std::strerror(errno)));
         }
-
-        std::vector<unsigned char> buffer(MAX_CHUNK_SIZE);
+	
+	float chunk[MAX_CHUNK_SIZE];
         std::size_t length;
-        while ((length = std::fread(&buffer[0], sizeof buffer[0], buffer.size(), stdin)) > 0) {
-            std::vector<unsigned char> chunk(buffer.begin(),
-                                             buffer.begin() + length);
-            process_chunk(chunk);
-        }
+        while ((length = std::fread(chunk, sizeof(float), MAX_CHUNK_SIZE, stdin)) > 0) {
+		if(length < MAX_CHUNK_SIZE){
+			memset(&chunk[length],0,MAX_CHUNK_SIZE-length);
+		}
+		while(true){
+			smileres_t result = smile_extaudiosource_write_data(handle, "externalAudioSource", (void*)chunk, MAX_CHUNK_SIZE);
+			if(result == SMILE_SUCCESS){
+				break;
+			}
+		}
+	}
     }
     catch (std::exception const& e) {
         std::cerr << e.what() << std::endl;
         return EXIT_FAILURE;
     }
+    
+    smile_extaudiosource_set_external_eoi(handle, "externalAudioSource");
+    thread_object.join();
     return EXIT_SUCCESS;
 }
 
-void process_chunk(const std::vector<unsigned char>& chunk) {
-    std::cout << chunk.size() << std::endl;
-}
