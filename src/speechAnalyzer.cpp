cdc7c004c3851c8ec760d931d4c0957bcc41bc59
Vincent Raymond <vraymond6@email.arizona.edu>
2021-02-17 14:40:15 -0700

    Fixed bugs in source code and configuration files

--- src/speechAnalyzer.cpp
+++ src/speechAnalyzer.cpp
@@ -4,23 +4,20 @@
 #include <cstring> //strerror
 #include <iostream>
 #include <vector>
-
-const size_t MAX_CHUNK_SIZE = 2048;
-
-
+#include <fstream>
+const size_t MAX_NUM_SAMPLES = 512;
+void parse_audio_data(smileobj_t* handle);
 int main() {
 
     //Initialize and start opensmile
     smileobj_t* handle;
-    smileopt_t options[1];
-
-    options[0].name = "O";
-    options[0].value = "opensmile.energy.csv";
 
     handle = smile_new();
-    smile_initialize(handle, "external_audio_test.conf", 1, options, 1, 0, 1, 0);
+    smile_initialize(handle, "conf/external_audio_test.conf", 0, NULL, 1, 0, 1, 0);
     std::thread thread_object(smile_run, handle);
+    std::thread thread_object2(parse_audio_data, handle);
 
+    /*
     try {
         std::freopen(nullptr, "rb", stdin); // reopen stdin in binary mode
 
@@ -28,14 +25,14 @@ int main() {
             throw(std::runtime_error(std::strerror(errno)));
         }
 	
-	float chunk[MAX_CHUNK_SIZE];
+	float chunk[MAX_NUM_SAMPLES];
         std::size_t length;
-        while ((length = std::fread(chunk, sizeof(float), MAX_CHUNK_SIZE, stdin)) > 0) {
-		if(length < MAX_CHUNK_SIZE){
-			memset(&chunk[length],0,MAX_CHUNK_SIZE-length);
+        while ((length = std::fread(chunk, sizeof(float), MAX_NUM_SAMPLES, stdin)) > 0) {
+		if(length < MAX_NUM_SAMPLES){
+			memset(&chunk[length],0,(MAX_NUM_SAMPLES-length)*sizeof(float));
 		}
 		while(true){
-			smileres_t result = smile_extaudiosource_write_data(handle, "externalAudioSource", (void*)chunk, MAX_CHUNK_SIZE);
+			smileres_t result = smile_extaudiosource_write_data(handle, "externalAudioSource", (void*)chunk, length*sizeof(float));
 			if(result == SMILE_SUCCESS){
 				break;
 			}
@@ -49,6 +46,39 @@ int main() {
     
     smile_extaudiosource_set_external_eoi(handle, "externalAudioSource");
     thread_object.join();
-    return EXIT_SUCCESS;
+    return EXIT_SUCCESS;*/
+    thread_object.join();
+    thread_object2.join();
+    return 0;
+}
+
+void parse_audio_data(smileobj_t* handle){
+    try {
+        std::freopen(nullptr, "rb", stdin); // reopen stdin in binary mode
+
+        if (std::ferror(stdin)) {
+            throw(std::runtime_error(std::strerror(errno)));
+        }
+	
+	float chunk[MAX_NUM_SAMPLES];
+        std::size_t length;
+        while ((length = std::fread(chunk, sizeof(float), MAX_NUM_SAMPLES, stdin)) > 0) {
+		if(length < MAX_NUM_SAMPLES){
+			memset(&chunk[length],0,(MAX_NUM_SAMPLES-length)*sizeof(float));
+		}
+		while(true){
+			smileres_t result = smile_extaudiosource_write_data(handle, "externalAudioSource", (void*)chunk, length*sizeof(float));
+			if(result == SMILE_SUCCESS){
+				break;
+			}
+		}
+	}
+    }
+    catch (std::exception const& e) {
+        std::cerr << e.what() << std::endl;
+    }
+    
+    smile_extaudiosource_set_external_eoi(handle, "externalAudioSource");
+
 }
 
