0ce1e35b529db064ac9e1d17b77ff33ef554106a
Vincent Raymond <vraymond6@arizona.edu>
2021-03-08 11:54:38 -0700

    Prototpe for asr streaming with speechAnalyzer

--- src/JsonBuilder.cpp
+++ src/JsonBuilder.cpp
@@ -1,19 +1,14 @@
-#include "SMILEapi.h"
-
-#include "json.hpp"
-
-#include <string>
-#include <cstring>
-#include <iostream>
 class JsonBuilder{
 
 	public:
 	JsonBuilder(){
-		this->j["lld"] = {};
-		this->j["tmeta"] = {};
+		this->j["header"] = {};
+		this->j["msg"] = {};
+		this->j["data"] = {};
+		this->j["data"]["features"]["lld"] = {};
+		this->j["data"]["tmeta"] = {};
 
 	}
-
 	void process_message(smilelogmsg_t message){
 		std::string temp(message.text);
 		temp.erase(std::remove(temp.begin(), temp.end(), ' '), temp.end()); 
@@ -21,13 +16,14 @@ class JsonBuilder{
 			if(temp.find("lld") != std::string::npos){
 				std::cout << j << std::endl;
 				tmeta = false;
+				create_header();
 			}
 			if(tmeta){
 				auto equals_index = temp.find('=');
 				std::string field = temp.substr(0, equals_index);
 				double value = std::atof(temp.substr(equals_index+1).c_str());
 
-				this->j["tmeta"][field] = value;
+				this->j["data"]["tmeta"][field] = value;
 			}
 		}
 
@@ -37,7 +33,7 @@ class JsonBuilder{
 			std::string field = temp.substr(dot_index+1, equals_index-dot_index-1);
 			double value = std::atof(temp.substr(equals_index+1).c_str());
 
-			this->j["lld"][field] = value;
+			this->j["data"]["features"]["lld"][field] = value;
 		}
 		
 		if(temp.find("tmeta:") != std::string::npos){
@@ -49,4 +45,6 @@ class JsonBuilder{
 	bool tmeta = false;
 	nlohmann::json j;
 
-};
+	void create_header(){
+		std::string timestamp = boost::posix_time::to_iso_extended_string(boost::posix_time::microsec_clock::universal_time()) + "Z";
+		
---
+++ src/parse_arguments.cc
@@ -0,0 +1,62 @@
+// Copyright 2016 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "parse_arguments.h"
+#include <getopt.h>
+#include "google/cloud/speech/v1/cloud_speech.pb.h"
+
+using google::cloud::speech::v1::RecognitionConfig;
+
+char* ParseArguments(int argc, char** argv, RecognitionConfig* config) {
+  // Parse the bit rate from the --bitrate command line option.
+  static struct option long_options[] = {
+      {"bitrate", required_argument, nullptr, 'b'}, {nullptr, 0, nullptr, 0}};
+  config->set_language_code("en");
+  config->set_sample_rate_hertz(16000);  // Default sample rate.
+  int opt;
+  int option_index = 0;
+  while ((opt = getopt_long(argc, argv, "b:", long_options, &option_index)) !=
+         -1) {
+    switch (opt) {
+      case 'b':
+        config->set_sample_rate_hertz(atoi(optarg));
+        if (0 == config->sample_rate_hertz()) return nullptr;
+        break;
+      default: /* '?' */
+        return nullptr;
+    }
+  }
+  if (optind == argc) {
+    // Missing the audio file path.
+    return nullptr;
+  }
+  // Choose the encoding by examining the audio file extension.
+  char* ext = strrchr(argv[optind], '.');
+  if (ext == nullptr || 0 == strcasecmp(ext, ".raw")) {
+    config->set_encoding(RecognitionConfig::LINEAR16);
+  } else if (0 == strcasecmp(ext, ".ulaw")) {
+    config->set_encoding(RecognitionConfig::MULAW);
+  } else if (0 == strcasecmp(ext, ".flac")) {
+    config->set_encoding(RecognitionConfig::FLAC);
+  } else if (0 == strcasecmp(ext, ".amr")) {
+    config->set_encoding(RecognitionConfig::AMR);
+    config->set_sample_rate_hertz(8000);
+  } else if (0 == strcasecmp(ext, ".awb")) {
+    config->set_encoding(RecognitionConfig::AMR_WB);
+    config->set_sample_rate_hertz(16000);
+  } else {
+    config->set_encoding(RecognitionConfig::LINEAR16);
+  }
+  return argv[optind];
+}
