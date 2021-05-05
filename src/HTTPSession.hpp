1cd2c2a78cb698afa439b77187aabeb60cd46d1f
Vincent Raymond <vraymond6@arizona.edu>
2021-05-05 15:20:04 -0700

    Fixed bug with files that include util.h

--- src/HTTPSession.hpp
+++ src/HTTPSession.hpp
@@ -7,7 +7,7 @@
 
 namespace http = beast::http; // from <boost/beast/http.hpp>
 namespace asio = boost::asio; // from <boost/asio.hpp>
-
+using namespace std;
 // Return a reasonable mime type based on the extension of a file.
 beast::string_view mime_type(beast::string_view path) {
     using beast::iequals;
