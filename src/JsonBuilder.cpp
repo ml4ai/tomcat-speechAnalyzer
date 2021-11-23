e7fa45c6142247524a461c77d2f82018403c2ded
Vincent Raymond <vraymond6@arizona.edu>
2021-11-23 15:12:06 -0500

    Updated version number

--- src/JsonBuilder.cpp
+++ src/JsonBuilder.cpp
@@ -422,7 +422,7 @@ nlohmann::json JsonBuilder::create_common_msg(std::string sub_type) {
     message["timestamp"] = timestamp;
     message["experiment_id"] = GLOBAL_LISTENER.experiment_id;
     message["trial_id"] = GLOBAL_LISTENER.trial_id;
-    message["version"] = "3.0.3";
+    message["version"] = "3.1.0";
     message["source"] = "tomcat_speech_analyzer";
     message["sub_type"] = sub_type;
 
